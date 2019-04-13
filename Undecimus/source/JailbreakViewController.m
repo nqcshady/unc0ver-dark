87b75d5713060b0f8408975886a552cbc440db3d
Diatrus <pinseeker75@outlook.com>
2019-04-13 00:16:36 -0400

    Delete old u0 sileo

--- Undecimus/source/JailbreakViewController.m
+++ Undecimus/source/JailbreakViewController.m
@@ -1835,6 +1835,10 @@ dictionary[@(name)] = ADDRSTRING(value); \
         }
         
         // Dpkg better work now
+        if (pkgIsInstalled("org.juulstar.sileo")){
+            removePkg("org.juulstar.sileo", true);
+        }
+            
         if (pkgIsBy("Sam Bingner", "apt1.4")) {
             removePkg("apt1.4", true);
         }
